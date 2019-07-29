12276323db61ec6d398ed66fe5e7a02aaafd2bb9
paolagc <pgarcia@cafetosoftware.com>
2019-07-29 17:24:32 -0500

    Added intro exercises

--- python_training/initial_exercises/repeated_letters.py
+++ python_training/initial_exercises/repeated_letters.py
@@ -4,6 +4,8 @@ from string import ascii_lowercase
 from python_training.utilities.string_utilities import english_word_list
 
 word_list = english_word_list()
+
+# get all the letters not appearing double in any word
 for letter in ascii_lowercase: # all letters
     exists = False
     for word in word_list:
@@ -13,3 +15,21 @@ for letter in ascii_lowercase: # all letters
 
     if not exists:
         print("There are not english words containing twice the letter : {}".format(letter))
+
+
+# getting all the words containing uu
+for word in word_list:
+        if 'uu' in word:
+            print(word)
+
+# getting all the words containing all vowels
+for word in word_list:
+    contains = True
+    for v in list('aeiou'):
+        if v not in word:
+            contains = False
+            break
+    
+    if contains:
+        print('The word {} contains all the vowels'.format(word))
+        
