071bf51a0e7bd7f663c3ed8090d0c8c98f582abb
paolagc <pgarcia@cafetosoftware.com>
2019-07-31 17:40:15 -0500

    Find all the palindromes

--- python_training/initial_exercises/repeated_letters.py
+++ python_training/initial_exercises/repeated_letters.py
@@ -1,9 +1,11 @@
 
 from string import ascii_lowercase
+from math import floor
 
 from python_training.utilities.string_utilities import english_word_list
 
 word_list = english_word_list()
+
 
 # get all the letters not appearing double in any word
 for letter in ascii_lowercase: # all letters
@@ -32,4 +34,24 @@ for word in word_list:
     
     if contains:
         print('The word {} contains all the vowels'.format(word))
+
+# get all palindromes
+def is_palindrome(word):
+    l = len(word)
+    if(l > 2):
+        m = floor(l /2)
+        l -= 1
+        i = 0
+        while(i<m):
+            if(word[i] == word[l - i]):
+                i += 1
+            else:
+                break
+        return i == m
+    else:
+        return False
+        
+for word in word_list:
+    if(is_palindrome(word)):
+        print(word)
         
