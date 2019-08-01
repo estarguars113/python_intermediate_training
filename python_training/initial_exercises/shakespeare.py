2a156f82972d6fbf512260640fd4f854828aa918
paolagc <paola@atomic-hub.com>
2019-07-31 22:09:10 -0500

    Basic shakespeare analysis

---
+++ python_training/initial_exercises/shakespeare.py
@@ -0,0 +1,23 @@
+from python_training.utilities.string_utilities import english_word_list
+from python_training.utilities.string_utilities import shakespeare_sonnets
+
+
+import timeit
+
+# get the total of novel words
+# (words in the sonnets but not contained in the word list)
+shakespeare = shakespeare_sonnets()
+all_text = ' '.join(shakespeare)
+word_list = english_word_list()
+all_words = ''.join(word_list)
+
+counter = 0
+novel_words = []
+start_time = timeit.default_timer()
+for word in shakespeare:
+    if(word not in all_words):
+        counter += 1
+        novel_words.append(word)
+
+print(counter, novel_words)
+print(timeit.default_timer() - start_time)
--- python_training/utilities/string_utilities.py
+++ python_training/utilities/string_utilities.py
@@ -1,15 +1,18 @@
 import json
 
-"""
-    Get a list of the english words
-"""
+
 def english_word_list() -> list:
-    '''
-    path_file = './python_training/utilities/assets/words_dictionary.json'
-    with open(path_file) as json_file:
-        words = json.load(json_file)
-        return list(words.keys())
-    '''
+    """
+        Get a list of the english words
+    """
     path_file = './python_training/utilities/assets/sowpods.txt'
     txt_file = open(path_file)
     return [x.rstrip('\n').lower() for x in txt_file]
+
+
+def shakespeare_sonnets() -> list:
+    """
+        Get a list containing shakespeare sonnets
+    """
+    path_file = './python_training/utilities/assets/sonnets.txt'
+    return [x.lower() for l in open(path_file).readlines() for x in set(l.strip().split(' '))]
